c111f3164173a04b20a312f6ecc71e28ab15a8f8
Francis Yang <francis.yang@uchicago.edu>
2020-08-15 14:00:24 +0800

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        if hardness == "Soft" {
+            print(softTime)
+        }else if hardness == "Medium"{print(mediumTime)
+        }else {print(hardTime)
+        }
+    }
+    
+    
 
 }
+
