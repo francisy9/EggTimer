8fd92b64d43c797a1b8028c3298d47b1500030b3
Francis Yang <francis.yang@uchicago.edu>
2020-08-17 14:32:04 +0800

    Completed the app. Added progress bar to represent progress, as well as an alarm sound when the egg finishes.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,39 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var textLabel: UILabel!
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
-    
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        
-        let hardness = sender.currentTitle!
-        
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("error")
-        }
-        
-        
-        /*Alternatively, use if statements
-         
-         if hardness == "Soft" {
-            print(softTime)
-        }else if hardness == "Medium"{print(mediumTime)
-        }else {print(hardTime)
-        }*/
-    }
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+       
+        var totalTime = 0
+        var timePassed = 0
+        var timer = Timer()
+        var player : AVAudioPlayer!
     
     
+       
+       @IBAction func buttonPressed(_ sender: UIButton) {
+        progressBar.progress = 0.0
+           timer.invalidate()
+           let hardness = sender.currentTitle!
+           totalTime =  eggTimes[hardness]!
+        timePassed = 0
+        textLabel.text = "You are now making \(hardness) eggs!"
 
-}
+             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+           }
+
+
+           @objc func updateCounter() {
+               //example functionality
+               if timePassed<totalTime {
+                   timePassed += 1
+                progressBar.progress = Float(timePassed)/Float(totalTime)
+               } else if timePassed == totalTime {
+                timer.invalidate()
+                   textLabel.text = "Done"
+                playSound(sound: "alarm_sound")
+                
+               }
+           }
+    func playSound(sound: String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    }
 
